43c0da32843bc42f401e8d35af870ba8e9aa3dbe
nicolascorderodev <nicolascordero281@gmail.com>
2024-10-12 21:55:25 -0500

    Iniciando repositorio

---
+++ python/hello.py
@@ -0,0 +1,37 @@
+# Esto es un comentario de una sola linea.
+
+'''
+Esto es
+un comentario
+de
+varias
+lineas
+'''
+
+"""
+Esto es un
+comentario 
+con
+comillas 
+dobles
+"""
+
+print("Hola Python.")
+
+print('esto es un string')
+
+x = 5 #TIPO DE DATO ENTERO POSITIVO(INTEGER)
+
+x = -5#TIPO DE DATO ENTERO NEGATIVO(INTEGER)
+
+x = -2.5#TIPO DE DATO FLOAT NEGATIVO(FLOAT)
+
+x = 2.5#TIPO DE DATO FLOAT POSITIVO(FLOAT)
+
+x = "String"#CADENA DE CARACTERES(STRING)
+
+x = True#BOOLEANO(VERDADERO)
+
+x = False#BOOLEANO(FALSO)
+
+print(x)
