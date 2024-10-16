a2ba7835890f66483305be8852fea1417523ef82
nicolascorderodev <nicolascordero281@gmail.com>
2024-10-15 22:15:29 -0500

    avanzando

--- python/00_hello.py
+++ python/00_hello.py
@@ -20,18 +20,15 @@ print("Hola Python.")
 
 print('esto es un string')
 
+#TIPOS DE DATOS
 x = 5 #TIPO DE DATO ENTERO POSITIVO(INTEGER)
-
 x = -5#TIPO DE DATO ENTERO NEGATIVO(INTEGER)
-
 x = -2.5#TIPO DE DATO FLOAT NEGATIVO(FLOAT)
-
 x = 2.5#TIPO DE DATO FLOAT POSITIVO(FLOAT)
-
-x = "String"#CADENA DE CARACTERES(STRING)
-
+x = "String@l-//*//*()"#CADENA DE CARACTERES(STRING)
 x = True#BOOLEANO(VERDADERO)
-
 x = False#BOOLEANO(FALSO)
 
-print(x)
+
+#Consultar el tipo de dato
+print(type(True)) #Tipo 'str'
---
+++ python/01_variables.py
@@ -0,0 +1,18 @@
+### Variables ###
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+
+my_float_variable = 1.22
+print(my_float_variable)
+
+my_int_to_str_variable = int(my_float_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# Concatenación de variables en un print
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
