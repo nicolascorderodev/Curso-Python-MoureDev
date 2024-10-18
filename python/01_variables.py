e3d337a288ba75999232421cef530236b30f6337
nicolascorderodev <nicolascordero281@gmail.com>
2024-10-17 22:51:44 -0500

    Terminamos la clase de variables y comenzamos con operadores

--- python/01_variables.py
+++ python/01_variables.py
@@ -23,6 +23,29 @@ first_name = "Nicolas"
 print("Primer nombre: ", len(first_name))
 print("Primer nombre: ", first_name)
 
-#Variables en una sola línea
-name, surname, alias, age = "Nicolas", "Cordero", "Nico", 20
+#Variables en una sola línea -- ¡Cuidado con abusar de esta sintaxis!
+name, surname, alias, age = "Nicolas", "Cordero", "Nico", int(20.1)
 print("Me llamo:", name, surname, "Mi edad es:", age, "Y mi alias es:", alias)
+print(type(age))
+
+#Inputs
+name = input('¿Cuál es tu nombre? ')
+age = input('¿Cuántos años tienes? ')
+deporte = input('Cuál es tu deporte favorito? ')
+print(name)
+print(age)
+
+print("Hola",name,"Tienes",age,"años.")
+print("Tu deporte favorito es:", deporte)
+
+#Cambiamos su tipo
+name = 20
+age = "Nicolas"
+print(name)
+print(age)
+
+# ¿Forzamos el tipo?
+direccion: str = "carrera 10b 20-44"
+direccion = 30
+direccion: float = 20.2
+print(type(direccion))
---
+++ python/02_operadores.py
@@ -0,0 +1,22 @@
+### Operadores Aritméticos ###
+
+# Operaciones con enteros
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+# Operaciones con cadenas de texto
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
+
+# Operaciones mixtas
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(2.5 * 2))
